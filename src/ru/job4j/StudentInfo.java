5ab40953536b89b974daf20137324a010be196e7
SoloShur <iFSAMNfHW23AtmS>
2021-05-03 10:00:42 +0300

    3.3. Разбор первой программы. [#159033]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Alexandr Solopov");
+        System.out.println("06.02.1994");
+    }
+}
