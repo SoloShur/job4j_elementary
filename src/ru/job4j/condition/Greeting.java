b90229f2dc4539014a26452866238d4cc224db3c
SoloShur <iFSAMNfHW23AtmS>
2021-05-05 10:08:05 +0300

    9.1. String [#237407 #623]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -2,9 +2,11 @@ package ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
-        int year = 2021;
-        String idea = "I like Java!" + " But I am a newbie." + year;
+        String idea = "I like Java!";
         System.out.println(idea);
-
+        System.out.println(idea + " But I am a newbie");
+        int year = 2021;
+        idea = "I like Java! " + year;
+        System.out.println(idea);
     }
 }
